3cba449733006596b269af1788d123bd72230f6d
rkd0812 <qorakqneo760>
2020-07-03 07:28:23 +0900

    - Test Case 작성 중 - Lists Controller Test

--- src/test/java/com/kcnet/todosv/lists/ListControllerTest.java
+++ src/test/java/com/kcnet/todosv/lists/ListControllerTest.java
@@ -1,4 +1,29 @@
 package com.kcnet.todosv.lists;
 
-public class ListControllerTest {
+import com.kcnet.todosv.common.BaseControllerTest;
+import org.junit.Test;
+import org.springframework.hateoas.MediaTypes;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class ListControllerTest extends BaseControllerTest {
+
+    @Test
+    public void createListTest() throws Exception {
+        ListsDto listsDto = ListsDto.builder()
+                            .boardId("B001")
+                            .position(65535)
+                            .title("test lists")
+                            .build();
+        mockMvc.perform(
+                post("/lists")
+                .contentType(MediaTypes.ALPS_JSON_VALUE)
+                .accept(MediaTypes.HAL_JSON)
+                .content(this.objectMapper.writeValueAsString(listsDto)))
+           .andDo(print())
+           .andExpect(status().isCreated());
+    }
+
 }
